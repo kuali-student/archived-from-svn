b2074985ced36d884cad7450299bed7df83e6a88
nwright <nwright@kuali.org>
2012-09-19 21:22:29 -0400

    KSENROLL-2863 POC to Explore a Generated Admin UI more work on getting unit tests for remote impls so I can run these admin uis just saving
    git-svn-id: https://svn.kuali.org/repos/student/poc@36452 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-standalone-admin-app/src/main/java/org/kuali/student/service/remote/impl/RemoteServiceConstants.java
+++ ks-standalone-admin-app/src/main/java/org/kuali/student/service/remote/impl/RemoteServiceConstants.java
@@ -15,5 +15,5 @@ public class RemoteServiceConstants
  public static final String ENV2_URL = "http://env2.ks.kuali.org";
  public static final String STAGING_EMBEDDED_URL = "http://staging.ks.kuali.org"; 
  public static final String DEV_EMBEDDED_URL = "http://dev.ks.kuali.org";
- public static final String LOCAL_HOST_EMBEDDED_URL = "http://localhost:9393/ks-embedded-dev";
+ public static final String LOCAL_HOST_EMBEDDED_URL = "http://localhost:8080/ks-embedded-2.0.0-SNAPSHOT";
 }
