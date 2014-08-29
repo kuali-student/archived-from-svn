ad55d1df6b480d6a42c4dddfa2e6ada542bb51a7
llfourie <llfourie@kuali.org>
2014-08-29 02:53:02 -0400

    KSENROLL-14423 : Refactored the Authorization AFTs for the new UI on Admin Registration
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@75004 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/step_definitions/az_central_registration_personnel_steps.rb
+++ features/step_definitions/az_central_registration_personnel_steps.rb
@@ -4,5 +4,6 @@ When(/^I navigate to Admin Registration$/) do
 end
 
 Then /^I have access to select a student$/ do
-  on(AdminRegistration).student_info_go_btn.present?.should be_true
+  on(AdminRegistration).student_id_input.present?.should be_true
+  on(AdminRegistration).student_term_go_button.present?.should be_true
 end
