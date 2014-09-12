6dae2e9ec7f0b2ed433b34d65bdb850087105650
peggy <peggy@kuali.org>
2014-09-12 05:51:25 -0400

    KSENROLL-14588: Apply hold page, feature and step files
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/sambal/trunk@75899 0ac02d80-fb3f-11dc-a574-c57244a850eb

---
+++ features/step_definitions/apply_hold_steps.rb
@@ -0,0 +1,7 @@
+When(/^I apply a hold to student by completing the required information needed for applied hold$/) do
+
+end
+
+Then(/^the applied hold exists in the student applied hold list$/) do
+
+end
---
+++ lib/sambal-kuali/pages/registration/holds/apply_hold_page.rb
@@ -0,0 +1,43 @@
+class ApplyHold < BasePage
+
+  wrapper_elements
+  frame_element
+  validation_elements
+  ######################################################################################################################
+  ###                                           Apply Hold Constants                                       ###
+  ######################################################################################################################
+
+
+  ######################################################################################################################
+  ###                                            Apply Hold Section                                         ###
+  ######################################################################################################################
+  element(:apply_hold_view) { |b| b.frm.div(id: "KS-AppliedHoldMaintenanceView")}
+
+
+  element(:apply_hold_page) { |b| b.main(id: "KS-Hold-Apply-Page")}
+  element(:apply_hold_section) { |b| b.apply_hold_page.div(id: "KS-ApplyHold-HoldSection")}
+
+
+  ######################################################################################################################
+  ###                                      Apply Hold Input Fields                                         ###
+  ######################################################################################################################
+  element(:hold_code) { |b| b.apply_hold_section.text_field(id: /holdCode/)}
+  element(:first_applied_date) { |b| b.apply_hold_section.text_field(id: /effectiveDate/)}
+
+  element(:first_term) { |b| b.apply_hold_section.text_field(id: /firstTerm/)}
+  ######################################################################################################################
+  ###                                            Apply Hold Buttons                                         ###
+  ######################################################################################################################
+  element(:apply_hold_show_btn) { |b| b.apply_hold_section.button(id: "show_button")}
+  action(:apply_hold_show){ |b| b.apply_hold_show_btn.when_present.click}
+  element(:apply_hold__btn) { |b| b.apply_hold_section.button(id: "applyHoldButton")}
+  action(:apply_hold){ |b| b.apply_hold__btn.when_present.click}
+
+
+
+
+  ################################################################
+  ### Apply Holds error Methods
+  #################################################################
+
+end
