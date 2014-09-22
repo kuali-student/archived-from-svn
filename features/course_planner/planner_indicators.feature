e5e4eb83a32d34c95a89b45e050f5fcd42fdd881
sanujkumar <sanujkumar@kuali.org>
2014-09-22 07:23:23 -0400

    KSAP-1422
    git-svn-id: https://svn.kuali.org/repos/student/test/functional-automation/ksap-aft/trunk@76546 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- features/course_planner/planner_indicators.feature
+++ features/course_planner/planner_indicators.feature
@@ -9,5 +9,19 @@ Feature: BT.Planner Indicators and Real Time Data
     And I navigate to the Planner page
     Then the course should indicate that it is not offered for the term
 
+  Scenario: VP 5.0.2 Verify the indicator for a course that has retired with a past expiration date
+    When I navigate to the planner page to view a planned course that has retired with a past expiration date
+    Then the course should indicate that it is not offered for the term
+
+  Scenario: VP 5.0.3 Verify the indicator for a course planned for suspended or cancelled registration groups
+    When I navigate to the planner page to view a course planned for suspended or cancelled registration groups
+    Then the course should indicate that the registration group has been suspended or cancelled
+
+
+  Scenario: VP 5.0.4 Verify the indicator for a course planned for suspended or cancelled course offering
+    When I navigate to the planner page to view a course planned for suspended or cancelled course offering
+    Then the course should indicate that the registration group has been cancelled or suspended
+
+
 
 
--- features/step_definitions/planner_indicators_steps.rb
+++ features/step_definitions/planner_indicators_steps.rb
@@ -9,7 +9,42 @@ end
 Then(/^the course should indicate that it is not offered for the term$/) do
  on CoursePlannerPage do |page|
    page.course_indicator(@course_search_result.course_code,@course_search_result.planned_term,@course_search_result.state)
-   puts "#{@course_search_result.course_code} is not scheduled for #{@course_search_result.term}"
    page.indicator_popup.should match "#{@course_search_result.course_code} is not scheduled for #{@course_search_result.term}"
  end
+end
+
+
+
+When(/^I navigate to the planner page to view a planned course that has retired with a past expiration date$/) do
+  @course_search_result = make CourseSearchResults, :course_code => "BSCI353",:planned_term=>"2014Spring",:state=>"planned",:term=> "Spring 2014"
+  @course_search_result.course_search
+  navigate_to_course_planner_home
+
+end
+
+
+
+When(/^I navigate to the planner page to view a course planned for suspended or cancelled registration groups$/) do
+  @course_search_result = make CourseSearchResults, :course_code => "CHEM271",:planned_term=>"2014Spring",:state=>"planned",:term=> "Spring 2014"
+  @course_search_result.course_search
+  navigate_to_course_planner_home
+end
+
+
+  Then(/^the course should indicate that the registration group has been suspended or cancelled$/) do
+    page.course_indicator(@course_search_result.course_code,@course_search_result.planned_term,@course_search_result.state)
+    page.indicator_popup.should match "#{@course_search_result.course_code} has been suspended"
+  end
+
+When(/^I navigate to the planner page to view a course planned for suspended or cancelled course offering$/) do
+  @course_search_result = make CourseSearchResults, :course_code => "ENGL388",:planned_term=>"2014Spring",:state=>"planned",:term=> "Spring 2014"
+  @course_search_result.course_search
+  navigate_to_course_planner_home
+end
+
+
+Then(/^the course should indicate that the registration group has been cancelled or suspended$/) do
+  page.course_indicator(@course_search_result.course_code,@course_search_result.planned_term,@course_search_result.state)
+  page.indicator_popup.should match "#{@course_search_result.course_code} has been cancelled"
+
 end
